de3c739e55f0f8a27617ee128ef445a32e56da9b
D1r3Wolf <d1r3wolf.aj@gmail.com>
2019-04-05 20:49:59 +0530

    writeups

--- Encrypt-2019/pwn0/exp.py
+++ Encrypt-2019/pwn0/exp.py
@@ -1,7 +1,10 @@
 from pwn import *
 def connect(server=1):
 	if server:
-		return remote("104.154.106.182","1234")
+		try:
+			return remote("104.154.106.182","1234")
+		except:
+			print "[-] The server is down : (Try offline) `con = connect(0)` on line 12"
 	else:
 		return process("./pwn0")
 
